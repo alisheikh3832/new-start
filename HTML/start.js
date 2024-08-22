8fdda3dea39f1a9d4b1ef04041d9ff1a48776fde
alisheikh3832 <alisheikh3832@gmail.com>
2024-08-22 13:34:26 -0700

    22-8-24

---
+++ HTML/start.js
@@ -0,0 +1,14 @@
+// // prompt( "CheckBox"  )
+// const checkbox = document.getElementById('myCheckbox');
+
+// checkbox.addEventListener('change', function() {
+//   if (this.checked) {
+//     alert('Checkbox is checked!');
+//   } else {
+//     alert('Checkbox is unchecked!');
+//   }
+// });
+alert("Error! enter a valid password.") 
+alert( "Welcome to Js Land \n Happy Coding" )
+alert( "Hello.. I can run JS through my web browser console" )
+// prompt("Danger Territory")
